22fbaed88bd57ecde1d3276df77e3c5a0ef95ac1
Manny <manny@Zipcoders-MacBook-Pro-58.local>
2023-09-02 15:40:01 -0400

    form with button need to work on posting to h2

---
+++ PaperPlane/src/main/resources/public/app.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const quill = new Quill('#editor', {
+        theme: 'snow'
+    });
+
+    document.getElementById('postForm').addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const postContent = quill.root.innerHTML;
+        const post = { content: postContent };
+
+        fetch('/api/**', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(post)
+        })
+            .then(response => response.json())
+            .then(data => {
+                alert('Post saved successfully!');
+                // Optionally, reset the form or perform other actions
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                alert('Error saving post.');
+            });
+    });
+});
+
