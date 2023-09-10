651aebd371736ef489cdfa4701ccd30169a79d62
BrentC0328 <107273004+BrentC0328@users.noreply.github.com>
2023-09-09 20:09:56 -0400

    remove extra PaperPlane inception

--- PaperPlane/PaperPlane/src/main/java/zipcode/paperplane/repository/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package zipcode.paperplane.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import zipcode.paperplane.domain.Authority;
-
-/**
- * Spring Data JPA repository for the {@link Authority} entity.
- */
-public interface AuthorityRepository extends JpaRepository<Authority, String> {}
