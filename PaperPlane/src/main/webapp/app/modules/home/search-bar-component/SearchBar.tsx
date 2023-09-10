743f90b3e776c348465746eefb5d4fe52eb21c49
Dmytro_Romaniv <128440171+dimaromaniv@users.noreply.github.com>
2023-09-10 17:19:31 -0400

    add clear button

--- PaperPlane/src/main/webapp/app/modules/home/search-bar-component/SearchBar.tsx
+++ PaperPlane/src/main/webapp/app/modules/home/search-bar-component/SearchBar.tsx
@@ -1,47 +1,111 @@
-  import React, { useState, useEffect } from 'react';
-  import axios from 'axios';
-  import 'react-quill/dist/quill.snow.css';
-  import './SearchBar.css';
+//   import React, { useState, useEffect } from 'react';
+//   import axios from 'axios';
+//   import 'react-quill/dist/quill.snow.css';
+//   import './SearchBar.css';
+//
+//   const SearchBar = ({ setResults }) => {
+//     const [input, setInput] = useState('');
+//
+//      const fetchData = (value) => {
+//     fetch(`http://localhost:8080/api/documents?eagerload=false`)
+//       .then((response) => response.json())
+//       .then((json) => {
+//
+//         const results = json.filter((document) => {
+//             return (
+//             value &&
+//             document &&
+//             document.title &&
+//             document.title.toLowerCase().includes(value)
+//             );
+//         });
+//          console.log(results);
+//          setResults(results);
+//
+//       });
+//   };
+//
+//      const handleChange = (value) => {
+//        setInput(value);
+//        fetchData(value);
+//      };
+//
+//      const handleClear = () => {
+//        setInput(''); // Clear the input field
+//        setResults([]); // Optionally, clear the search results as well
+//      };
+//
+//      return (
+//  <div className="search-bar-container">
+//    <input
+//      className="search-input"
+//      type="text"
+//      placeholder="Search for a document by title"
+//      value={input}
+//      onChange={(e) => handleChange(e.target.value)}
+//    />
+//    {input && ( // Render the button only if there is text in the input
+//      <button
+//        className="clear-button"
+//        onClick={handleClear}
+//      >
+//        Clear
+//      </button>
+//    )}
+//  </div>)
+//
+//    export default SearchBar;
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import 'react-quill/dist/quill.snow.css';
+import './SearchBar.css';
 
-  const SearchBar = ({ setResults }) => {
-    const [input, setInput] = useState('');
+const SearchBar = ({ setResults }) => {
+  const [input, setInput] = useState('');
 
-     const fetchData = (value) => {
+  const fetchData = (value) => {
     fetch(`http://localhost:8080/api/documents?eagerload=false`)
       .then((response) => response.json())
       .then((json) => {
-
         const results = json.filter((document) => {
-            return (
+          return (
             value &&
             document &&
             document.title &&
             document.title.toLowerCase().includes(value)
-            );
+          );
         });
-         console.log(results);
-         setResults(results);
-
+        console.log(results);
+        setResults(results);
       });
   };
 
-     const handleChange = (value) => {
-       setInput(value);
-       fetchData(value);
-     };
+  const handleChange = (value) => {
+    setInput(value);
+    fetchData(value);
+  };
 
-     return (
-       <div className="search-bar-container">
-         <input
-           className="search-input"
-           type="text"
-           placeholder="Search for a document by title"
-           value={input}
-           onChange={(e) => handleChange(e.target.value)}
-         />
-       </div>
-     );
-   };
+  const handleClear = () => {
+    setInput(''); // Clear the input field
+    setResults([]); // Optionally, clear the search results as well
+  };
 
+  return (
+    <div className="search-bar-container">
+      <input
+        className="search-input"
+        type="text"
+        placeholder="Search for a document by title"
+        value={input}
+        onChange={(e) => handleChange(e.target.value)}
+      />
+      {input && ( // Render the button only if there is text in the input
+        <button className="clear-button" onClick={handleClear}>
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
 
-   export default SearchBar;
+export default SearchBar;
--- PaperPlane/src/main/webapp/app/modules/home/search-bar-component/SearchResultsList.tsx
+++ PaperPlane/src/main/webapp/app/modules/home/search-bar-component/SearchResultsList.tsx
@@ -1,4 +1,29 @@
-import React, {useState, useEffect, useRef} from 'react';
+// import React, {useState, useEffect, useRef} from 'react';
+// import SearchResult from "./SearchResult";
+//
+// interface SearchResultsListProps {
+//   results: any[]; // Adjust the type to match the actual data type of results
+//   onSelectDocument: (selectedDocument: any) => void;
+// }
+//
+// const SearchResultsList: React.FC<SearchResultsListProps> = ({ results, onSelectDocument }) => {
+// // Check if there are results or if the search bar is empty
+//   if (!results.length) {
+//     return null; // Return null to render nothing
+//     }
+//   return (
+//     <div className="results-list">
+//       {results.map((result, id) => (
+//         <SearchResult result={result} key={id} onSelectDocument={onSelectDocument} />
+//       ))}
+//     </div>
+//   );
+// };
+//
+//
+//    export default SearchResultsList;
+//
+import React from 'react';
 import SearchResult from "./SearchResult";
 
 interface SearchResultsListProps {
@@ -7,6 +32,11 @@ interface SearchResultsListProps {
 }
 
 const SearchResultsList: React.FC<SearchResultsListProps> = ({ results, onSelectDocument }) => {
+  // Check if there are results to display
+  if (!results.length) {
+    return null; // Return null to render nothing when there are no results
+  }
+
   return (
     <div className="results-list">
       {results.map((result, id) => (
@@ -16,6 +46,4 @@ const SearchResultsList: React.FC<SearchResultsListProps> = ({ results, onSelect
   );
 };
 
-
-   export default SearchResultsList;
-
+export default SearchResultsList;
